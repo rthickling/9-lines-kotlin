95f9ed5620c15eb00d5c32a5896b407c54a37d8b
Richard Hickling <richard@profitview.net>
2020-10-15 21:12:04 +0100

    Cutting to 7 lines

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,14 +3,6 @@ import com.github.nkzawa.socketio.client.Socket
 
 fun main() {
     val socket = IO.socket("https://markets.profitview.net?api_key=223834ffcc42d292c3a5fdc156c244ad2b020465")
-    socket.connect().on(Socket.EVENT_CONNECT) {
-        socket.emit(
-            "subscribe",
-            arrayOf("trade:bitmex:XBTUSD", "trade:bitmex:ETHUSD")
-        )
-    }
-    socket.on("trade") { parameters ->
-        for(parameter in parameters)
-            println(parameter)
-    }
+    socket.connect().on(Socket.EVENT_CONNECT) { socket.emit("subscribe", arrayOf("trade:bitmex:XBTUSD", "trade:bitmex:ETHUSD"))}
+    socket.on("trade") { parameters -> for(parameter in parameters) println(parameter)}
 }
